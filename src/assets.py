72c7424a761abb67260305b17ce58ca6987734c6
cody-mckeon <codymckeon@gmail.com>
2025-05-16 08:52:22 -0700

    Update assets.py

--- src/assets.py
+++ src/assets.py
@@ -1,1 +1,9 @@
+# src/assets.py
+
+def enrich_assets_with_yf_symbols(assets_dict):
+    for k, v in assets_dict.items():
+        ticker = v.get("micro_ticker") or v.get("ticker")
+        if ticker:
+            v["yf_symbol"] = f"{ticker}=F"
+    return assets_dict
 
