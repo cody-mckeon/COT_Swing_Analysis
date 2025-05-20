9e0e5ff0c48cf135220715e7e503c4c916c301e4
cody-mckeon <codymckeon@gmail.com>
2025-05-20 15:56:12 -0700

    Update cot_fetcher.py
    Update function name and fix bug

--- src/cot_fetcher.py
+++ src/cot_fetcher.py
@@ -12,7 +12,7 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-def get_active_liquid_contracts(date: str = '2024-04-23', oi_threshold: int = 200_000) -> pd.DataFrame:
+def fetch_cot_snapshot(date: str = '2024-04-23', oi_threshold: int = 200_000) -> pd.DataFrame:
     df = nasdaqdatalink.get_table("QDL/FON", date=date)
     """
     Fetches COT data and filters for contracts with:
@@ -32,7 +32,7 @@ def get_active_liquid_contracts(date: str = '2024-04-23', oi_threshold: int = 20
     active_contract = grouped[grouped.abs().sum(axis=1) > 0].index
     
     df['open_interest'] = df['total_reportable_longs'] + df['non_reportable_longs']
-    liquid_df = df[df['contract_code'].isin(valid_codes)]
+    liquid_df = df[df['contract_code'].isin(active_contract)]
     liquid_df = liquid_df[liquid_df['open_interest'] > oi_threshold]
 
     summary = liquid_df.groupby('contract_code')['open_interest'].sum().sort_values(ascending=False)
