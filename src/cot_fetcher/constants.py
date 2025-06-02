9ad766c70fece6d889eb1a557a556b3b1bc0b444
cody-mckeon <codymckeon@gmail.com>
2025-06-02 15:04:10 -0700

    Create constants.py

---
+++ src/cot_fetcher/constants.py
@@ -0,0 +1,7 @@
+# src/cot_fetcher/constants.py
+KEY_TRADER_COLUMNS = [
+    'money_manager_longs', 'money_manager_shorts',
+    'swap_dealer_longs', 'swap_dealer_shorts',
+    'producer_merchant_processor_user_longs',
+    'producer_merchant_processor_user_shorts',
+]
