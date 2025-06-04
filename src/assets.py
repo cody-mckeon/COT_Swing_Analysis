919900a86d785f3410c70865665cc5362d2f2b43
cody-mckeon <codymckeon@gmail.com>
2025-06-04 14:15:25 -0700

    Delete src/assets.py

--- src/assets.py
+++
@@ -1,9 +0,0 @@
-# src/assets.py
-
-def enrich_assets_with_yf_symbols(assets_dict):
-    for k, v in assets_dict.items():
-        ticker = v.get("micro_ticker") or v.get("ticker")
-        if ticker:
-            v["yf_symbol"] = f"{ticker}=F"
-    return assets_dict
-
