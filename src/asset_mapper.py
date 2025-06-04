14a970a24e137f2856b9c2cb4e98cf737a23d47c
cody-mckeon <codymckeon@gmail.com>
2025-06-04 14:15:17 -0700

    Delete src/asset_mapper.py

--- src/asset_mapper.py
+++
@@ -1,6 +0,0 @@
-# src/asset_mapper.py
-def merge_contract_names(summary_df: pd.DataFrame, path_to_excel: str) -> pd.DataFrame:
-    df_strikes = pd.read_excel(path_to_excel)[['ContractName', 'ContractMarketCode']].dropna()
-    df_strikes = df_strikes.drop_duplicates()
-    return summary_df.merge(df_strikes, left_on='contract_code', right_on='ContractMarketCode', how='left')
-
